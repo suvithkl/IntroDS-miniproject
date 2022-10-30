ef20c8f1656b2220c14af6925ae5afdf3be7e47d
suvithkl <suvi.t.hakala@helsinki.fi>
2022-10-30 15:33:15 +0200

    testing

---
+++ app/test.py
@@ -0,0 +1,28 @@
+import geopandas as gpd
+import folium
+
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+
+m = world.explore(
+    column="pop_est",  # make choropleth based on "BoroName" column
+    scheme="naturalbreaks",  # use mapclassify's natural breaks scheme
+    legend=True, # show legend
+    k=10, # use 10 bins
+    legend_kwds=dict(colorbar=False), # do not use colorbar
+    name="countries" # name of the layer in the map
+)
+
+cities.explore(
+    m=m, # pass the map object
+    color="red", # use red color on all points
+    marker_kwds=dict(radius=10, fill=True), # make marker radius 10px with fill
+    tooltip="name", # show "name" column in the tooltip
+    tooltip_kwds=dict(labels=False), # do not show column label in the tooltip
+    name="cities" # name of the layer in the map
+)
+
+folium.TileLayer('Stamen Toner', control=True).add_to(m)  # use folium to add alternative tiles
+folium.LayerControl().add_to(m)  # use folium to add layer control
+
+m  # show map
